455352439b086ca78788ca1ff932f7923eaf7784
mvelascoUTC <meliza-vech@hotmail.com>
2016-10-30 21:34:51 -0500

    archivos para realizar

---
+++ gruumay/Guiia/app/src/main/java/com/example/windows/guiia/Tab1.java
@@ -0,0 +1,19 @@
+package com.example.windows.guiia;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by windows on 30/10/2016.
+ */
+
+public class Tab1 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View rootview = inflater.inflate(R.layout.tab1, container,false);
+        return rootview;
+    }
+}
